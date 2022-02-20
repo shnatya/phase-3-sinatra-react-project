4028c2f11f01c4b4c4c30515f3bd038298721e86
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-19 22:12:12 -0800

    Create post and delete actions for jokes table

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,26 @@
+require 'pry'
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your project!" }.to_json
+  get "/jokes" do
+    jokes = Joke.all.order(question: :ASC)
+    jokes.to_json
+  end
+
+  post "/jokes" do
+    joke = Joke.create(
+      question: params[:question],
+      answer: params[:answer],
+      user_id: params[:user_id]
+    )
+    joke.to_json
+  end
+
+  delete "/jokes/:id" do
+    joke = Joke.find(params[:id])
+    joke.destroy
+    joke.to_json
   end
 
 end
