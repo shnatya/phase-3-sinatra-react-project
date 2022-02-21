f2808571e2c46115d4f4decc722a2e32e216b239
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-20 16:38:25 -0800

    In post route create new instance in joke_category connecting new joke and category

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -1,22 +1,28 @@
+require "pry"
 class JokesController < ApplicationController
 
     get "/jokes" do
-        jokes = Joke.all.order(question: :ASC)
-        jokes.to_json(include: {user: {only: [:username]}})
-      end
+        jokes = Joke.all
+        jokes.to_json(include: :user)
+    end
     
-      post "/jokes" do
-        joke = Joke.create(
-          question: params[:question],
-          answer: params[:answer],
-          user_id: params[:user_id]
-        )
+    post "/jokes" do
+        user = User.find_or_create_by(username: params[:user][:username])
+        joke = user.jokes.create(params[:joke])
         joke.to_json
-      end
+
+        category = Category.find_or_create_by(category_name: params[:category][:category_name])
+        joke_category = JokeCategory.create(
+            joke_id: joke.id,
+            category_id: category.id
+        )
+        joke_category.to_json
+    end
     
-      delete "/jokes/:id" do
+    delete "/jokes/:id" do
         joke = Joke.find(params[:id])
         joke.destroy
         joke.to_json
-      end
+    end
 end
+
