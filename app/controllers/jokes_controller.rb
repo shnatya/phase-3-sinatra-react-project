12c7f8e16a22c4389f6fd2ee28229dad5cd14daa
Anastasia Shatokhina <shnatya@gmail.com>
2022-02-22 22:58:58 -0800

    Change get route to /jokes/:category_id. Get jokes through association category has many jokes

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -3,17 +3,14 @@ class JokesController < ApplicationController
 
     get "/jokes" do
         jokes = Joke.all
-        jokes.to_json(include: :user)
+        jokes.to_json(only: [:question, :answer], include: {users: {only: :username}})
     end
 
-    get "/jokes/:category_name" do
-        capitalized_category(params[:category_name]) #private method?
-
+    get "/jokes/:category_id" do
         array_jokes = []
-        category = Category.find_by(category_name: @capitalized_category)
-        array_joke_categories = JokeCategory.where(category_id: category.id)
-        array_joke_categories.each do |j_c|
-            array_jokes << {question: j_c.joke.question, answer: j_c.joke.answer, username: j_c.joke.user.username}
+        category = Category.find(params[:category_id])
+        category.jokes.each do |joke|
+            array_jokes << {question: joke.question, answer: joke.answer, username: joke.user.username}
         end
         array_jokes.to_json
     end
@@ -26,7 +23,8 @@ class JokesController < ApplicationController
         array_joke_category_instances = []
         array_categories = params[:category][:category_name].split(/, /)
         array_categories.each do |category|
-            category_instance = Category.find_or_create_by(category_name: category)
+            category_instance = Category.find_or_create_by(category_name: category) #need to send
+            #category name or new category name
             joke_category = JokeCategory.create(
                 joke_id: joke.id,
                 category_id: category_instance.id)
@@ -40,12 +38,7 @@ class JokesController < ApplicationController
         joke = Joke.find(params[:id])
         joke.destroy
         joke.to_json
-    end
 
-    private
-
-    def capitalized_category(category)
-        @capitalized_category = category.capitalize()
     end
 end
 
